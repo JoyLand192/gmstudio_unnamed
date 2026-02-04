864a15fd2355f098e985e6578ef145ff8e2491a6
cyw46421@gmail.com <cyw46421@gmail.com>
2026-02-04 19:31:09 +0900

    캐릭터 이동 추가, 코드 개선 예정

--- Assets/Scripts/CRMovement.cs
+++ Assets/Scripts/CRMovement.cs
@@ -1,18 +1,44 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class CRMovement : MonoBehaviour
 {
-    CharacterController controller;
-    float moveSpeed;
+    [SerializeField] bool isGrounded;
+    [SerializeField] float jumpForce;
+    public float JumpForce
+    { 
+        get => jumpForce;
+        set => jumpForce = value;
+    }
+    [SerializeField] float moveSpeed;
     public float MoveSpeed
     {
         get => moveSpeed;
         set => moveSpeed = value;
     }
-    bool isGrounded;
+    [SerializeField] float gravityValue;
+    public float GravityValue
+    {
+        get => gravityValue;
+        set
+        {
+            gravityValue = value;
+        }
+    }
+    [SerializeField] Vector3 velocity = new();
+    CharacterController controller;
+    int currentJumpCount;
+    int maxJumpCount = 1;
+    public int MaxJumpCount
+    {
+        get => maxJumpCount;
+        set => maxJumpCount = value;
+    }
+    float jumpBoost = 1;
     void Awake()
     {
         controller = GetComponent<CharacterController>();
@@ -21,16 +47,37 @@ public class CRMovement : MonoBehaviour
     {
         isGrounded = controller.isGrounded;
 
-        float dX, dZ;
+        float dX, dZ = 0; ;
 
-        if (Input.GetKey(KeyCode.W)) dX = 1;
-        else if (Input.GetKey(KeyCode.S)) dX = -1;
+        if (Input.GetKey(KeyCode.D)) dX = 1;
+        else if (Input.GetKey(KeyCode.A)) dX = -1;
         else dX = 0;
 
-        if (Input.GetKey(KeyCode.D)) dZ = 1;
-        else if (Input.GetKey(KeyCode.A)) dZ = -1;
+        if (Input.GetKey(KeyCode.W)) dZ = 1;
+        else if (Input.GetKey(KeyCode.S)) dZ = -1;
         else dZ = 0;
 
-        controller.Move(new Vector3(dX * moveSpeed, 0, dZ * moveSpeed));
+        dX *= moveSpeed;
+        dZ *= moveSpeed;
+
+        var camDirectionX = Camera.main.transform.right;
+        var camDirectionZ = Camera.main.transform.forward;
+        camDirectionX.y = 0;
+        camDirectionZ.y = 0;
+        camDirectionX.Normalize();
+        camDirectionZ.Normalize();
+
+        if (!isGrounded) velocity.y -= gravityValue * Time.deltaTime;
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentJumpCount <= 0) return;
+            velocity.y = jumpForce * jumpBoost;
+            currentJumpCount--;
+        }
+        else if (isGrounded) currentJumpCount = MaxJumpCount;
+
+        controller.Move((velocity + camDirectionX * dX + camDirectionZ * dZ) * Time.deltaTime);
     }
 }
